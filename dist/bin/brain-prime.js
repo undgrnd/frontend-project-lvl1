49a0b8a0524c4c14a465ba8858b16785fbc8f87f
y.mikhin <y.mikhin@fun-box.ru>
2019-09-30 13:31:48 +0300

    Добавление description и keywords в package.json.

---
+++ dist/bin/brain-prime.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+"use strict";
+
+var _brainPrime = _interopRequireDefault(require("../games/brain-prime"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _brainPrime["default"])();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tcHJpbWUuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOzs7O0FBRUEiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5pbXBvcnQgYnJhaW5QcmltZSBmcm9tICcuLi9nYW1lcy9icmFpbi1wcmltZSc7XG5cbmJyYWluUHJpbWUoKTtcbiJdfQ==
---
+++ src/games/brain-prime.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+import { getRandomNumber } from '../helpers';
+import game from '../game';
+
+const RULES_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isNumberPrime = (number) => {
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const askQuestion = () => {
+  const number = getRandomNumber(1, 100);
+  const correctAnswer = isNumberPrime(number) ? 'yes' : 'no';
+  const userAnswer = readlineSync.question(`Question: ${number} `);
+
+  return {
+    isUserRight: correctAnswer === userAnswer,
+    userAnswer,
+    correctAnswer,
+  };
+};
+
+export default () => {
+  game(RULES_DESCRIPTION, askQuestion);
+};
