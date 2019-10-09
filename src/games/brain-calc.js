8fe3a58a32a3e087b72bdec0708062de317272a4
y.mikhin <y.mikhin@fun-box.ru>
2019-10-09 14:21:13 +0300

    Исправление дефолтного случая в brain-calc.

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -24,7 +24,7 @@ const askQuestion = () => {
       correctAnswer = number1 * number2;
       break;
     default:
-      correctAnswer = number1 + number2;
+      correctAnswer = null;
   }
 
   const userAnswer = parseInt(readlineSync.question(`Question: ${number1} ${operationOfQuestion} ${number2} `), 10);
