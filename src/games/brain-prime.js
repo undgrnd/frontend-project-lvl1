b27dd349e9d3a41c937575888d58c7f484e3f5a2
y.mikhin <y.mikhin@fun-box.ru>
2019-10-09 14:22:50 +0300

    Исправление проверки в функции isNumberPrime.

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,7 +5,7 @@ import game from '../game';
 const GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isNumberPrime = (number) => {
-  if (number === -1 || number === 0 || number === 1) return false;
+  if (number <= 1) return false;
   for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
       return false;
