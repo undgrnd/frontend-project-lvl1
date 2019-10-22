ed3b069dc5c18b721d01d29f9a227822ccf178f2
y.mikhin <y.mikhin@fun-box.ru>
2019-10-22 19:05:19 +0300

    Переименование переменной gameDescription > progressionLength.

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,12 +2,12 @@ import getRandomNumber from '../get-random-number';
 import game from '../game';
 
 const gameDescription = 'What number is missing in the progression?';
-const PROGRESSION_LENGTH = 10;
+const progressionLength = 10;
 
 const normalizeUserAnswer = (answer) => parseInt(answer, 10);
 
 const createQuestion = () => {
-  const progression = [...Array(PROGRESSION_LENGTH).keys()];
+  const progression = [...Array(progressionLength).keys()];
   const correctAnswer = progression[getRandomNumber(0, progression.length - 1)];
   progression[progression.indexOf(correctAnswer)] = '..';
 
