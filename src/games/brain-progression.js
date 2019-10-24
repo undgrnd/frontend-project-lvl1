ce911585e6d36dcf13d059fd54c92ea03e7d7319
y.mikhin <y.mikhin@fun-box.ru>
2019-10-25 00:59:55 +0300

    Изменение функции getQuestionAndAnswer игры BrainProgression.

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,22 +4,10 @@ import game from '../game';
 const gameDescription = 'What number is missing in the progression?';
 const progressionLength = 10;
 
-const createQuestion = () => {
-  const progression = [...Array(progressionLength).keys()];
-  const correctAnswer = progression[getRandomNumber(0, progression.length - 1)];
-  progression[progression.indexOf(correctAnswer)] = '..';
-
-  return {
-    progression: progression.join(' '),
-    correctAnswer,
-  };
-};
-
 const getQuestionAndAnswer = () => {
-  const {
-    progression,
-    correctAnswer: answer,
-  } = createQuestion();
+  const progression = [...Array(progressionLength).keys()];
+  const answer = progression[getRandomNumber(0, progression.length - 1)];
+  progression[progression.indexOf(answer)] = '..';
 
   return {
     question: progression,
