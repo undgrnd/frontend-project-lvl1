1bfb0b63707aa9944bd18a589143ea10bb67c7ae
y.mikhin <y.mikhin@fun-box.ru>
2019-10-26 23:15:27 +0300

    Редактирование сообщения в случае проигрыша.

--- src/game.js
+++ src/game.js
@@ -20,6 +20,7 @@ export default (rule, getQuestionAndAnswer) => {
       console.log('Correct!');
     } else {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}.`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
